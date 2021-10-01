520fba383c6bf87c5a57b00977b205ab983efe11
Jorge Grimaldo <JorgeGS1991@users.noreply.github.com>
2021-10-01 16:08:53 -0500

    Part 1 Done
    Done with part 1, fixed the input and output errors

--- HW3 ITIN8000_Jorge/HW3part1.py
+++ HW3 ITIN8000_Jorge/HW3part1.py
@@ -5,30 +5,50 @@
 # Add the user to a CSV file named UserData.csv in the order Last Name, First Name, Favorite Color, Days Old
 
 from datetime import date
+import pandas as pd
+
 
 # Ask for the users first and last name, favorite color, and date of birth
-
 UfName = input(" What's your Fist Name: ")
 UlName = input(" What's your Last Name: ")
 UfColor = input(" What's your Favorite Color? ")
 
-# Calculate Days old
-bd_m = int(input("What MONTH were you born: "))
-bd_d = int(input("what DAY were you born: "))
-bd_y = int(input("What YEAR were you born: "))
-
-now = date.today()
-
-age = date(int(bd_y), int(bd_m), int(bd_d))
-print("Your age is ", now - age)
+# Ask user for birthday information
+bd_m = int(input(" What MONTH (MM) were you born: "))
+bd_d = int(input(" What DAY (DD) were you born: "))
+bd_y = int(input(" What YEAR (YYYY) were you born: "))
 
 # Transfer Fname, LName and FColor to UserName.txt file
 with open('UserName.txt', 'w') as f:
     f.write('{} {} {}'.format(UlName, UfName, UfColor))
 
+
+# Calculate user's days old
+def calculateAge(birthDate):
+    age = int((date.today() - birthDate).days)
+    return age
+
+
+days = calculateAge(date(bd_y, bd_m, bd_d))
+
+# print(days, "Days Old")
+# Days old to Binary
+dOld = format(int(days), 'b')
+
 # Transfer user's age in days to a binary file "DaysOld"
+with open('DaysOld', 'w') as d:
+    d.write('{}'.format(dOld))
 
-temp = format(int(11095), 'b')
+# Add the user to a CSV file named UserData.csv
+# in the order Last Name, First Name, Favorite Color, Days Old
+data = {
+    'Last Name': [UlName],
+    'First Name': [UfName],
+    'Favorite Color': [UfColor],
+    'Days Old': [dOld]
+}
 
-print(temp)
+df = pd.DataFrame(data)
+df.to_csv("UserData.csv")
 
+print("\n\n Thank you for Participating")
